d266d93f32674164ef94ade9951c2ab9f7c63d55
abhinav-mishra <abhishu29@gmail.com>
2016-03-14 21:09:10 -0700

    Fixed task creation time

--- CPU_handling/task_creation_time/create_process.py
+++ CPU_handling/task_creation_time/create_process.py
@@ -1,5 +1,5 @@
 from subprocess import Popen,PIPE
 avg_times = 0
-for i in xrange(1000):
+for i in range(1000):
 	avg_times += int(Popen(['./create_process'],stdout=PIPE).communicate()[0][:-1])
-print str(avg_times/1000)
+print (str(avg_times/1000))
